b60be649f6a887ced2c07f24e3021ed47b9fbfd0
BillyLis-Murphy-STUD <G00465240@atu.ie>
2026-09-23 15:00:01 +0100

    Changed PurchaseOrder to Entity

--- src/main/java/ie/atu/cicd1orderservice/Model/PurchaseOrder.java
+++ src/main/java/ie/atu/cicd1orderservice/Model/PurchaseOrder.java
@@ -1,5 +1,6 @@
 package ie.atu.cicd1orderservice.Model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="PurchaseOrders")
 public class PurchaseOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long productId;
     private int quantity;
